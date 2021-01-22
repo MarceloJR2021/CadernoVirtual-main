d630dff62c61ef3f93ba172e94c51dddd9e46eeb
MarceloJR2021 <77733112+MarceloJR2021@users.noreply.github.com>
2021-01-22 14:05:47 -0300

    Initial commit

---
+++ CadernoVirtual.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void telaMenu(void);
+
+int main(void) {
+    telaMenu();
+    return 0;
+
+}
+
+
+void telaMenu(void) {
+    printf("\n");
+    printf("*****************************************************************************\n");
+    printf("**      Projeto Caderno Virtual de Receitas Culinarias                     **\n");
+    printf("**      Desenvolvedor - Marcelo Jailson de Araujo Junior                   **\n");
+    printf("**                                                                         **\n");
+    printf("*****************************************************************************\n");
+    printf("**                                                                         **\n");
+    printf("**      --------- Caderno Virtual de Receitas Culinarias ---------         **\n");
+    printf("**                                                                         **\n");
+    printf("**  Programa projeto de um caderno virtual de receitas culinarias com      **\n");
+    printf("**  a finalidade de Cadastrar,pesquisar,seja por nome,ingredientes,        **\n");
+    printf("**  localidade,custo ou outro,adicionando ou removendo como o usuario      **\n");
+    printf("**  desejar.Em resumo e um sistema capaz de gerenciar um conjunto de       **\n");
+    printf("**  receitas culinarias,por meio da busca por pratos que contenham         **\n");
+    printf("**  determinados ingredientes e a respectiva otimizacao dos estoques.      **\n");
+    printf("**                                                                         **\n");
+    printf("*****************************************************************************\n");
+    printf("\n");
+}
