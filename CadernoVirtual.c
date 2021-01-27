ecde4b9248fcf7b25fe25fce39f0d77554871752
MarceloJR2021 <77733112+MarceloJR2021@users.noreply.github.com>
2021-01-27 09:41:05 -0300

    Update CadernoVirtual.c
    Adicionando telas de menu(parte 1)

--- CadernoVirtual.c
+++ CadernoVirtual.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+void menuPrincipal(void);
+void menuReceitas(void);
+void menuEstoque(void);
+void menuFinancas(void);
+void telaCadastrarReceitas(void);
 void telaSobre(void);
 
 int main(void) {
     telaSobre();
+    menuPrincipal();
+    menuReceitas();
+    menuFinancas();
     return 0;
 
 }
-
 
 void telaSobre(void) {
     printf("\n");
@@ -29,3 +37,75 @@ void telaSobre(void) {
     printf("*****************************************************************************\n");
     printf("\n");
 }
+
+void menuPrincipal(void) {
+    system("cls");
+	printf("\n");
+	printf("***************************************************************************\n");
+	printf("**                                                                       **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**           ---------------  Menu Principal ---------------             **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**                                                                       **\n");
+	printf("**           1. Modo Receitas                                            **\n");
+	printf("**           2. Modo Estoque                                             **\n");
+	printf("**           3. Modo Financas                                            **\n");
+    printf("**           4. Informacoes sobre o sistema                              **\n");
+	printf("**           0. Encerra o programa                                       **\n");
+	printf("**                                                                       **\n");
+	printf("**           Escolha a opcao desejada:                                   **\n");
+	printf("**                                                                       **\n");
+	printf("**                                                                       **\n");
+	printf("*************************************************************************//\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+void menuReceitas(void) {
+    system("cls");
+	printf("\n");
+	printf("***************************************************************************\n");
+	printf("**                                                                       **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**           ---------------  Menu Receitas  ---------------             **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**                                                                       **\n");
+	printf("**           1. Cadastrar Receitas                                       **\n");
+	printf("**           2. Encontrar Receitas                                       **\n");
+	printf("**           3. Atualizar Receitas                                       **\n");
+    printf("**           4. Deletar receitas                                         **\n");
+	printf("**           5. Ver Receitas-Somente nome                                **\n");
+	printf("**           0. Sair                                                     **\n");
+	printf("**                                                                       **\n");
+	printf("**           Escolha a opcao desejada:                                   **\n");
+	printf("**                                                                       **\n");
+	printf("*************************************************************************//\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+void menuFinancas(void) {
+    system("cls");
+	printf("\n");
+	printf("***************************************************************************\n");
+	printf("**                                                                       **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**           ---------------  Menu Financas  ---------------             **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**                                                                       **\n");
+	printf("**           1. Adicionar Financas                                       **\n");
+	printf("**           2. Remover   Financas                                       **\n");
+	printf("**           3. Atualizar Financas                                       **\n");
+    printf("**           4. Ver Financas                                             **\n");
+	printf("**           0. Sair                                                     **\n");
+	printf("**                                                                       **\n");
+	printf("**           Escolha a opcao desejada:                                   **\n");
+	printf("**                                                                       **\n");
+	printf("*************************************************************************//\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
