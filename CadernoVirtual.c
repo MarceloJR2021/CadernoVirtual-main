678495bf9488b93aa46abd848ffb375437aa4a1a
MarceloJR2021 <77733112+MarceloJR2021@users.noreply.github.com>
2021-01-30 10:13:46 -0300

    Update CadernoVirtual.c
    Adicionando telas de menu (parte 3)

--- CadernoVirtual.c
+++ CadernoVirtual.c
@@ -5,8 +5,19 @@ void menuPrincipal(void);
 void menuReceitas(void);
 void menuEstoque(void);
 void menuFinancas(void);
-void telaCadastrarReceitas(void);
+void CadastrarReceitas(void);
+void deletarReceitas(void);
+void encontrarReceitas(void);
+void atualizarReceitas(void);
+void verReceitas(void);
+void adicionarEstoque(void);
+void removerEstoque(void);
+void verEstoque(void);
+void adicionarFinancas(void);
+void removerFinancas(void);
+void verFinancas(void);
 void telaSobre(void);
+void menuInformacoes(void);
 
 int main(void) {
     telaSobre();
@@ -14,6 +25,18 @@ int main(void) {
     menuReceitas();
     menuFinancas();
 	menuEstoque();
+	menuInformacoes();
+	CadastrarReceitas();
+	deletarReceitas();
+	encontrarReceitas();
+	atualizarReceitas();
+	verReceitas();
+	adicionarEstoque();
+	removerEstoque();
+	verEstoque();
+	adicionarFinancas();
+	removerFinancas();
+	verFinancas();
     return 0;
 
 }
@@ -57,7 +80,7 @@ void menuPrincipal(void) {
 	printf("**           Escolha a opcao desejada:                                   **\n");
 	printf("**                                                                       **\n");
 	printf("**                                                                       **\n");
-	printf("*************************************************************************//\n");
+	printf("***************************************************************************\n");
 	printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
 	getchar();
@@ -81,7 +104,7 @@ void menuReceitas(void) {
 	printf("**                                                                       **\n");
 	printf("**           Escolha a opcao desejada:                                   **\n");
 	printf("**                                                                       **\n");
-	printf("*************************************************************************//\n");
+	printf("***************************************************************************\n");
 	printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
 	getchar();
@@ -103,11 +126,12 @@ void menuEstoque(void) {
 	printf("**                                                                       **\n");
 	printf("**           Escolha a opcao desejada:                                   **\n");
 	printf("**                                                                       **\n");
-	printf("*************************************************************************//\n");
+	printf("***************************************************************************\n");
 	printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
 	getchar();
 }
+
 void menuFinancas(void) {
     system("cls");
 	printf("\n");
@@ -124,9 +148,241 @@ void menuFinancas(void) {
 	printf("**                                                                       **\n");
 	printf("**           Escolha a opcao desejada:                                   **\n");
 	printf("**                                                                       **\n");
-	printf("*************************************************************************//\n");
+	printf("***************************************************************************\n");
 	printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
 	getchar();
 }
 
+void menuInformacoes(void) {
+    system("cls");
+	printf("\n");
+	printf("***************************************************************************\n");
+	printf("**                                                                       **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**           ---------------  Informacoes    ---------------             **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**                                                                       **\n");
+	printf("**           Quantidade de Receitas:                                     **\n");
+	printf("**           Quantidade de Itens no Estoque:                             **\n");
+    printf("**           Financas:                                                   **\n");
+	printf("**                                                                       **\n");
+	printf("**                                                                       **\n");
+	printf("***************************************************************************\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+void CadastrarReceitas(void) {
+    system("cls");
+	printf("\n");
+	printf("***************************************************************************\n");
+	printf("**                                                                       **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**           ------------  Cadastrar Receitas  -------------             **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**                                                                       **\n");
+	printf("**           Nome da Receita:                                            **\n");
+	printf("**           Ingredientes:                                               **\n");
+    printf("**           Modo de Preparo:                                            **\n");
+	printf("**           Origem da Receita(Pais ou Regiao):                          **\n");
+	printf("**           Local de Obtencao da Receita(Site,Livro,Revista):           **\n");
+	printf("**                                                                       **\n");
+	printf("**                                                                       **\n");
+	printf("**                                                                       **\n");
+	printf("***************************************************************************\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+void encontrarReceitas(void) {
+    system("cls");
+	printf("\n");
+	printf("***************************************************************************\n");
+	printf("**                                                                       **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**           ------------ Encontrar Receita por ------------             **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**                                                                       **\n");
+	printf("**           1. Nome:                                                    **\n");
+	printf("**           2. Ingrediente:                                             **\n");
+    printf("**           3. Origem:                                                  **\n");
+	printf("**           0. Voltar                                                   **\n");
+	printf("**                                                                       **\n");
+	printf("**                                                                       **\n");
+	printf("***************************************************************************\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+void atualizarReceitas(void) {
+    system("cls");
+	printf("\n");
+	printf("***************************************************************************\n");
+	printf("**                                                                       **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**           -------------- Atualizar Receita --------------             **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**                                                                       **\n");
+	printf("**           Nome da Receita para Atualizar:                             **\n");
+	printf("**                                                                       **\n");
+	printf("**                                                                       **\n");
+	printf("**                                                                       **\n");
+	printf("***************************************************************************\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+void deletarReceitas(void) {
+    system("cls");
+	printf("\n");
+	printf("***************************************************************************\n");
+	printf("**                                                                       **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**           -------------- Deletar Receita ----------------             **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**                                                                       **\n");
+	printf("**           Nome da Receita para Deletar:                               **\n");
+	printf("**                                                                       **\n");
+	printf("***************************************************************************\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+void verReceitas(void) {
+    system("cls");
+	printf("\n");
+	printf("***************************************************************************\n");
+	printf("**                                                                       **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**           --------------   Ver Receitas  ----------------             **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**                                                                       **\n");
+	printf("**           |Nome da Receita|                                           **\n");
+	printf("**                                                                       **\n");
+	printf("**                                                                       **\n");
+	printf("***************************************************************************\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+void adicionarEstoque(void) {
+    system("cls");
+	printf("\n");
+	printf("***************************************************************************\n");
+	printf("**                                                                       **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**           ------------- Adicionar Estoque ---------------             **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**                                                                       **\n");
+	printf("**           Item:                                                       **\n");
+	printf("**           Quantidade:                                                 **\n");
+	printf("**           Valor(R$):                                                  **\n");
+	printf("**                                                                       **\n");	
+	printf("***************************************************************************\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+void removerEstoque(void) {
+    system("cls");
+	printf("\n");
+	printf("***************************************************************************\n");
+	printf("**                                                                       **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**           -------------- Remover Estoque ----------------             **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**                                                                       **\n");
+	printf("**           Item:                                                       **\n");
+	printf("**           Quantidade:                                                 **\n");
+	printf("**                                                                       **\n");
+	printf("***************************************************************************\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+void verEstoque(void) {
+    system("cls");
+	printf("\n");
+	printf("***************************************************************************\n");
+	printf("**                                                                       **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**           ---------------   Ver Estoque  ----------------             **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**                                                                       **\n");
+	printf("**             Nome              |            Quantidade                 **\n");
+	printf("**                               |                                       **\n");
+	printf("**                               |                                       **\n");
+	printf("**                               |                                       **\n");
+	printf("**                               |                                       **\n");
+	printf("**                               |                                       **\n");
+	printf("**                               |                                       **\n");
+	printf("**                               |                                       **\n");
+	printf("**                               |                                       **\n");
+	printf("**                               |                                       **\n");
+	printf("**                                                                       **\n");			
+	printf("***************************************************************************\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+void adicionarFinancas(void) {
+    system("cls");
+	printf("\n");
+	printf("***************************************************************************\n");
+	printf("**                                                                       **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**           ------------- Adicionar Financas --------------             **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**                                                                       **\n");
+	printf("**           Valor(R$):                                                  **\n");
+	printf("**                                                                       **\n");
+	printf("***************************************************************************\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+void removerFinancas(void) {
+    system("cls");
+	printf("\n");
+	printf("***************************************************************************\n");
+	printf("**                                                                       **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**           -------------- Remover Financas ---------------             **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**                                                                       **\n");
+	printf("**           Valor(R$):                                                  **\n");
+	printf("**                                                                       **\n");
+	printf("***************************************************************************\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+void verFinancas(void) {
+    system("cls");
+	printf("\n");
+	printf("***************************************************************************\n");
+	printf("**                                                                       **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**           --------------   Ver Financas  ----------------             **\n");
+	printf("**           -----------------------------------------------             **\n");
+	printf("**                                                                       **\n");
+	printf("**           Valor das Financas:                                         **\n");
+	printf("**           R$                                                          **\n");
+	printf("**                                                                       **\n");
+	printf("***************************************************************************\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
