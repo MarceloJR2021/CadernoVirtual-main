d2dd9724b2b64a07e42b47923bb10756d80954cf
MarceloJR2021 <77733112+MarceloJR2021@users.noreply.github.com>
2021-03-24 11:16:05 -0300

    Projeto
    Parte 9 - Relatorio Ficticio

--- Relatorio.c
+++ Relatorio.c
@@ -1,6 +1,12 @@
 void relatorioReceitas(void) {
 	char op;
-    system("cls");
+	char quant[] = "21";
+	char nome[] = "Macarrao com Salsicha"; 
+	char Ingredientes[] = " 1 Unidade de Macarrao,5 unidades de Salsicha,\n             50 g de Manteiga,1 Unidade de molho\n";
+	char preparo[] = "Preparar o Macarrao,cortar as salsichas,temperar \n             o macarrao com molho e manteiga e jogar as salsichas\n";
+    char origem[] = "Italia";
+	char obtencao[] = "www.receitas.com.br";
+	system("cls");
 	printf("\n");
 	printf("***************************************************************************\n");
 	printf("**                                                                       **\n");
@@ -8,8 +14,13 @@ void relatorioReceitas(void) {
 	printf("**           ------------  Relatorio de Receitas -----------             **\n");
 	printf("**           -----------------------------------------------             **\n");
 	printf("**                                                                       **\n");
-	printf("**           Quantidade de Receitas:                                     **\n");
-	printf("**           Ultima receitas Adicionada:                                 **\n");
+	printf("**           Quantidade de Receitas: %s                                  **\n", &quant);
+	printf("**           Ultima receita Adicionada                                   **\n\n");
+	printf("             Nome:  %s \n\n",&nome);
+	printf("             Ingredientes:  %s \n",&Ingredientes);
+	printf("             Preparo:  %s \n",&preparo);
+	printf("             Origem:  %s \n\n",&origem);
+	printf("             Obtencao:  %s \n",&obtencao);
 	printf("**                                                                       **\n");
 	printf("**                                                                       **\n");
 	printf("***************************************************************************\n");
@@ -20,6 +31,8 @@ void relatorioReceitas(void) {
 
 void relatorioEstoque(void) {
 	char op;
+	char itens[] = "35 Itens";
+	char Item[] = "5 Unidades de Salsicha";
     system("cls");
 	printf("\n");
 	printf("***************************************************************************\n");
@@ -28,8 +41,9 @@ void relatorioEstoque(void) {
 	printf("**           ------------  Relatorio de Estoque ------------             **\n");
 	printf("**           -----------------------------------------------             **\n");
 	printf("**                                                                       **\n");
-	printf("**           Quantidade de Itens no Estoque:                             **\n");
-	printf("**           Ultima Iten Adicionado:                                     **\n");
+	printf("             Quantidade de Itens no Estoque: %s                           \n", &itens);
+	printf("**           Ultima Iten Adicionado                                      **\n");
+	printf("             Item : %s \n",&Item);
 	printf("**                                                                       **\n");
 	printf("**                                                                       **\n");
 	printf("***************************************************************************\n");
@@ -40,6 +54,9 @@ void relatorioEstoque(void) {
 
 void relatorioFinancas(void) {
 	char op;
+	char financas[] = "R$ 185,00";
+	char entrada[] = "R$ 35,50";
+	char saida[] = "R$ 15,50";
     system("cls");
 	printf("\n");
 	printf("***************************************************************************\n");
@@ -48,8 +65,9 @@ void relatorioFinancas(void) {
 	printf("**           ------------  Relatorio de Financas -----------             **\n");
 	printf("**           -----------------------------------------------             **\n");
 	printf("**                                                                       **\n");
-	printf("**           Financas:                                                   **\n");
-	printf("**           Ultima entrada Adicionada:                                  **\n");
+	printf("             Financas: %s                                                 \n\n",&financas);
+	printf("             Ultima entrada: %s                                     \n\n",&entrada);
+	printf("             Ultima Saida:   %s                                     \n",&saida);
 	printf("**                                                                       **\n");
 	printf("**                                                                       **\n");
 	printf("***************************************************************************\n");
